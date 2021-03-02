a544ee573aaa8fbdbc6aab69f04f3e5a7a5d4de9
Abhinav R <abhinav@sukra-helitek.com>
2021-03-02 16:15:29 +0530

    Added day1 solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    
+    let input = File::open("input.txt").unwrap();
+    let mut items : Vec<u32> = BufReader::new(input).lines().map(|line| line.unwrap().parse::<u32>().unwrap()).collect();
+
+    items.sort_unstable();
+
+    for a in &items {
+        for b in &items {
+            for c in &items {
+                if a + b + c == 2020 {
+                    println!("{}\n", a * b * c);
+                    return;
+                }
+            }
+        }
+    }
 }
