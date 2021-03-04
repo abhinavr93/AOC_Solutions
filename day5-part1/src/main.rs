7d2ca894bd745a3e8a4189e4d57910d68dbd17e9
abhinavr93 <rajagopalan.abhinav@gmail.com>
2021-03-05 02:20:48 +0530

    Added Day 5 Part 1 solution

--- day5-part1/src/main.rs
+++ day5-part1/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let input = File::open("input.txt").unwrap();
+    let mut line_iter = BufReader::new(input).lines();
+    let mut max_num = 0;
+    while let Some(Ok(line)) = line_iter.next() {
+        let bdigits: Vec<u32> = line
+            .chars()
+            .map(|p| match p {
+                'F' => 0,
+                'B' => 1,
+                'L' => 0,
+                'R' => 1,
+                _ => 0,
+            })
+            .collect();
+
+        let mut sum = 0;
+        for (i, digit) in bdigits.iter().enumerate() {
+            sum += digit * 2u32.pow((bdigits.len() - i - 1) as u32);
+        }
+
+        if sum > max_num {
+            max_num = sum;
+        }
+    }
+
+    println!("\n Max Seat Number: {} \n", max_num);
 }
