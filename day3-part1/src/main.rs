0ffadd51b2153a490dfe0ff5281eaa49f85bba51
abhinavr93 <rajagopalan.abhinav@gmail.com>
2021-03-04 20:32:36 +0530

    Added day 3 part 1 solution

--- day3-part1/src/main.rs
+++ day3-part1/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn get_xpos(i: usize, mx: usize, len: usize) -> usize {
+    ((i + 1) * mx) % len
+}
+
 fn main() {
-    println!("Hello, world!");
+    const MX: usize = 3;
+    const MY: usize = 1;
+
+    let input = File::open("input.txt").unwrap();
+    let mut line_iter = BufReader::new(input).lines().step_by(MY);
+    let line_len = line_iter.next().unwrap().unwrap().len();
+
+    let mut tree_count = 0;
+    let mut line_count = 0;
+
+    for (i, line) in line_iter.enumerate() {
+        let s = line.unwrap();
+        println!("{}", s);
+        let mut char_iter = s.chars();
+        if char_iter.nth(get_xpos(i, MX, line_len)).unwrap() == '#' {
+            tree_count += 1;
+        }
+
+        line_count += MY;
+    }
+
+    println!("\n The number of trees('#') found are : {} ", tree_count);
+    println!("\n Total number of lines : {}", line_count);
 }
