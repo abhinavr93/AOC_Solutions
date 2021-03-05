08f91cad8f7a056e47896224f49a7159ff99f5c4
Abhinav R <abhinav@sukra-helitek.com>
2021-03-05 13:31:24 +0530

    Added day 6 part 1 solution

--- day6-part1/src/main.rs
+++ day6-part1/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let group_iter = include_str!("../input.txt").split("\n\n");
+
+    let mut yes_vec: Vec<u32> = Vec::new();
+    let mut sum: u32 = 0;
+    for group in group_iter {
+        let mut unique_chars: Vec<char> = Vec::new();
+        for c in group.chars() {
+            if c != '\n' && !unique_chars.contains(&c) {
+                unique_chars.push(c);
+            }
+        }
+
+        yes_vec.push(unique_chars.len() as u32);
+        sum += unique_chars.len() as u32;
+    }
+
+    println!("Sum: {}", sum);
 }
