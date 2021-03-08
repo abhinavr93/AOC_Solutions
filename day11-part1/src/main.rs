558ef9d6c675ff61eed55022a8d331f1b2fc15a7
abhinavr93 <rajagopalan.abhinav@gmail.com>
2021-03-09 04:10:27 +0530

    Completed Day 11 part 1 solution (success)

--- day11-part1/src/main.rs
+++ day11-part1/src/main.rs
@@ -2,11 +2,12 @@ use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-struct State(Vec<Vec<char>>);
+#[derive(PartialEq)]
+struct State<'a>(&'a Vec<Vec<char>>);
 
-impl fmt::Display for State {
+impl fmt::Display for State<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for v in &self.0 {
+        for v in self.0 {
             for c in v {
                 write!(f, "{}", c)?;
             }
@@ -16,12 +17,77 @@ impl fmt::Display for State {
     }
 }
 
-fn get_neighbour_indices(row: usize, col: usize) -> Vec<(usize, usize)> {
+impl State<'_> {
+    fn get(&self, row: usize, col: usize) -> Option<char> {
+        if let Some(row_vec) = self.0.get(row) {
+            if let Some(char_val) = row_vec.get(col) {
+                return Some(*char_val);
+            }
+        }
+        None
+    }
+}
 
-	let indices: Vec<(usize, usize)> = Vec::new();
+fn get_new_seat(row: usize, col: usize, orig_seats: &Vec<Vec<char>>) -> Option<char> {
+    let orig_state = State(orig_seats);
+    let mut occupied_count = 0;
+    let mut start_row = row;
+    let mut start_col = col;
+    if row > 0 {
+        start_row = row - 1;
+    }
+    if col > 0 {
+        start_col = col - 1;
+    }
+    for i in start_row..=row + 1 {
+        for j in start_col..=col + 1 {
+            if let Some(char_val) = orig_state.get(i, j) {
+                if char_val == '#' && !(i == row && j == col) {
+                    occupied_count += 1;
+                }
+            }
+        }
+    }
 
-	indices
+    if let Some(char_val) = orig_state.get(row, col) {
+        if char_val == '#' && occupied_count >= 4 {
+            return Some('L');
+        } else if char_val == 'L' && occupied_count == 0 {
+            return Some('#');
+        } else {
+            return Some(char_val);
+        }
+    }
 
+    None
+}
+
+fn update_state(seats: &mut Vec<Vec<char>>) -> bool {
+    let orig_seats = seats.clone();
+    for i in 0..seats.len() {
+        for j in 0..seats[i].len() {
+            seats[i][j] = get_new_seat(i, j, &orig_seats).unwrap();
+        }
+    }
+
+    if State(seats) == State(&orig_seats) {
+        return false;
+    }
+
+    true
+}
+
+fn count_occupied(seats: &Vec<Vec<char>>) -> u32 {
+    let mut count: u32 = 0;
+    for i in 0..seats.len() {
+        for j in 0..seats[i].len() {
+            if seats[i][j] == '#' {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
 
 fn main() {
@@ -34,5 +100,17 @@ fn main() {
         seat_state.push(line.chars().collect::<Vec<char>>());
     }
 
-    println!("{}", State(seat_state));
+    println!("\nOriginal State:\n{}", State(&seat_state));
+
+    let mut iter = 0;
+
+    while update_state(&mut seat_state) {
+        iter += 1;
+        println!(
+            "After round {}, occupied seats: {}\n{}",
+            iter,
+            count_occupied(&seat_state),
+            State(&seat_state)
+        );
+    }
 }
