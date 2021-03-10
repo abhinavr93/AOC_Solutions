64a5ac9c8be21966553194a04832989643468c29
abhinavr93 <rajagopalan.abhinav@gmail.com>
2021-03-10 19:18:03 +0530

    Completed day 13 part 2 solution (success)

--- day13-part2/src/main.rs
+++ day13-part2/src/main.rs
@@ -1,7 +1,5 @@
 extern crate num;
 
-use num::bigint::BigInt;
-use num::Integer;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -11,41 +9,51 @@ fn main() {
 
     let second_line = line_iter.nth(1).unwrap().unwrap();
 
-    let ids: Vec<(usize, u32)> = second_line
-        .split(',')
-        .enumerate()
-        .filter_map(|(i, s)| {
-            if let Ok(x) = s.parse::<u32>() {
-                Some((i, x))
-            } else {
-                None
-            }
-        })
-        .collect();
+    let mut ids_iter = second_line.split(',').enumerate().filter_map(|(i, s)| {
+        if let Ok(x) = s.parse::<u32>() {
+            Some((i, x))
+        } else {
+            None
+        }
+    });
 
-    println!("IDs: {:?}", ids);
+    //println!("IDs: {:?}", ids_iter.collect::<Vec<(usize, u32)>>());
 
-    let n1: u64 = 221;
-    let n2: u64 = 19;
-    let offset: u64 = 3;
-    let prev_time: u64 = 102;
-    println!(
-        "Time for {} and {} with offset {} : {:?}",
-        n1,
-        n2,
-        offset,
-        find_time_at_offset(n1, n2, prev_time, offset)
-    );
+    let first_val = ids_iter.next().unwrap();
+    let mut prev_time: u64 = first_val.0 as u64;
+    let mut prev_lcm: u64 = first_val.1 as u64;
+
+    let second_val = ids_iter.next().unwrap();
+    let mut n2: u64 = second_val.1 as u64;
+    let mut offset: u64 = second_val.0 as u64;
+
+    let mut count = 2;
+    for id in ids_iter {
+        if let Some((time, lcm)) = find_time_at_offset(prev_time, prev_lcm, n2, offset) {
+            println!("Time for first {} buses : {:?}", count, time);
+
+            prev_time = time;
+            prev_lcm = lcm;
+            n2 = id.1 as u64;
+            offset = id.0 as u64;
+
+            count += 1;
+        }
+    }
+
+    if let Some((time, _lcm)) = find_time_at_offset(prev_time, prev_lcm, n2, offset) {
+        println!("Final Time for list with {} buses : {:?}", count, time);
+    }
 }
 
 // Will be faster if prev_lcm > n2. prev_lcm is intended to be the lcm of previous values
 // and prev_time is the previously found time.
-fn find_time_at_offset(prev_lcm: u64, n2: u64, prev_time: u64, offset: u64) -> Option<(u64, u64)> {
+fn find_time_at_offset(prev_time: u64, prev_lcm: u64, n2: u64, offset: u64) -> Option<(u64, u64)> {
     let new_lcm: u64 = num::Integer::lcm(&prev_lcm, &n2);
 
-    for time in (prev_lcm..=(new_lcm + prev_time)).rev().step_by(prev_lcm as usize) {
-        if (time + 1) % n2 == 0 {
-            return Some(((time * offset) % new_lcm, new_lcm));
+    for time in (prev_time..=(new_lcm + prev_time)).step_by(prev_lcm as usize) {
+        if (time + offset) % n2 == 0 {
+            return Some((time, new_lcm));
         }
     }
 
